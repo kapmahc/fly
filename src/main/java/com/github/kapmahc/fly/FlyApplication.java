10e39d015f14163a586e7a8de9d287cfc5a254c8
Jeremy Zheng <jitang.zheng@gmail.com>
2017-09-08 11:42:05 -0700

    create project

---
+++ src/main/java/com/github/kapmahc/fly/FlyApplication.java
@@ -0,0 +1,12 @@
+package com.github.kapmahc.fly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyApplication.class, args);
+	}
+}
