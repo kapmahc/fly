e4132f63708dbf1f324119646774d32f41cfa116
Jeremy Zheng <jitang.zheng@gmail.com>
2017-09-13 08:27:32 -0700

    add form

--- plugins/nut/c-user-must-sign-in.go
+++ plugins/nut/c-user-must-sign-in.go
@@ -2,6 +2,39 @@ package nut
 
 import "github.com/astaxie/beego/orm"
 
+// GetUsersProfile users'profile
+// @router /users/profile [get]
+func (p *Plugin) GetUsersProfile() {
+	p.LayoutDashboard()
+	p.Data[TITLE] = Tr(p.Locale(), "nut.users.profile.title")
+	p.TplName = "nut/users/profile.html"
+}
+
+// GetUsersChangePassword change user password
+// @router /users/change-password [get]
+func (p *Plugin) GetUsersChangePassword() {
+	p.LayoutDashboard()
+	p.Data[TITLE] = Tr(p.Locale(), "nut.users.change-password.title")
+	p.TplName = "nut/users/change-password.html"
+}
+
+// GetUsersLogs user logs
+// @router /users/logs [get]
+func (p *Plugin) GetUsersLogs() {
+	p.LayoutDashboard()
+	var items []Log
+	_, err := orm.NewOrm().QueryTable(new(Log)).
+		Filter("user_id", p.CurrentUser().ID).
+		OrderBy("-updated_at").
+		Limit(120).
+		All(&items)
+	p.Flash(nil, err)
+
+	p.Data[TITLE] = Tr(p.Locale(), "nut.users.logs.title")
+	p.Data["logs"] = items
+	p.TplName = "nut/users/logs.html"
+}
+
 // DeleteUsersSignOut user sign out
 // @router /users/sign-out [delete]
 func (p *Plugin) DeleteUsersSignOut() {
